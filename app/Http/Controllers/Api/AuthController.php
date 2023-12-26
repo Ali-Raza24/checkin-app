8e3ba82e399744ae934d72f75455b7d18ca99094
Ali <razamughaln@gmail.com>
2023-12-26 15:10:51 +0500

    add status code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         ]);
 
         if (!Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Invalid credentials.']);
+            return response()->json(['message' => 'Invalid credentials.'],401);
         }
 
         $user = $request->user();
